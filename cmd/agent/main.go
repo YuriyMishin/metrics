ada9cf104e913c06b2cf68a67b313ebcc09dff58
Yuriy Mishin <yuriy.mishin@gmail.com>
2025-03-30 23:50:38 +0300

    iter4

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -2,20 +2,51 @@ package main
 
 import (
 	"YuriyMishin/metrics/agent"
+	"flag"
+	"fmt"
 	"log"
 	"time"
 )
 
+func parseAgentFlags() (string, time.Duration, time.Duration, error) {
+	var (
+		addr           string
+		pollInterval   int
+		reportInterval int
+	)
+
+	flag.StringVar(&addr, "a", "localhost:8080", "HTTP server endpoint address")
+	flag.IntVar(&pollInterval, "p", 2, "Poll interval in seconds")
+	flag.IntVar(&reportInterval, "r", 10, "Report interval in seconds")
+
+	flag.Parse()
+
+	if flag.NArg() > 0 {
+		return "", 0, 0, fmt.Errorf("unknown flags: %v", flag.Args())
+	}
+
+	if pollInterval <= 0 {
+		return "", 0, 0, fmt.Errorf("poll интервал должен быть положительным")
+	}
+	if reportInterval <= 0 {
+		return "", 0, 0, fmt.Errorf("report интервал должен быть положительным")
+	}
+
+	return addr, time.Duration(pollInterval) * time.Second,
+		time.Duration(reportInterval) * time.Second, nil
+}
+
 func main() {
+	addr, pollInterval, reportInterval, err := parseAgentFlags()
+	if err != nil {
+		log.Fatalf("Error parsing flags: %v", err)
+	}
+
 	// Создаем отправитель с RESTy клиентом
-	sender := agent.NewRestySender("http://localhost:8080")
+	sender := agent.NewRestySender("http://" + addr)
 
 	// Настраиваем агент
-	agent := agent.NewAgent(
-		2*time.Second,  // Интервал сбора метрик
-		10*time.Second, // Интервал отправки
-		sender,
-	)
+	agent := agent.NewAgent(pollInterval, reportInterval, sender)
 
 	log.Println("Starting agent...")
 	agent.Run()
--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,10 +1,32 @@
 package main
 
-import "YuriyMishin/metrics/server"
+import (
+	"YuriyMishin/metrics/server"
+	"flag"
+	"fmt"
+	"log"
+)
+
+func parseServerFlags() (string, error) {
+	var addr string
+	flag.StringVar(&addr, "a", "localhost:8080", "HTTP server endpoint address")
+
+	flag.Parse()
+
+	if flag.NArg() > 0 {
+		return "", fmt.Errorf("unknown flags: %v", flag.Args())
+	}
+
+	return addr, nil
+}
 
 func main() {
+	addr, err := parseServerFlags()
+	if err != nil {
+		log.Fatalf("Error parsing flags: %v", err)
+	}
 	srv := server.NewServer()
-	if err := srv.Start("localhost:8080"); err != nil {
+	if err := srv.Start(addr); err != nil {
 		panic(err)
 	}
 }
